a3876fdba12ab05c9ce1e38fed71081562c3cb23
sonncho <dungcho93@gmail.com>
2023-04-27 23:37:31 +0900

    add: Selector components

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -7,6 +7,9 @@ import PersonIcon from '../../public/static/svg/auth/person.svg';
 import OpendEyeIcon from '../../public/static/svg/auth/opened_eye.svg';
 import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
 import Input from '../common/Input';
+import palette from '../../styles/palette';
+import Selector from '../common/Selector';
+import { dayList, monthList, yearList } from '../../lib/staticData';
 
 const ContainerForm = styled.form`
   width: 568px;
@@ -28,6 +31,34 @@ const ContainerForm = styled.form`
   .sign-up-password-input-wrapper {
     svg {
       cursor: pointer;
+    }
+  }
+
+  .sign-up-birthdate-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-birthday-info {
+    margin-bottom: 16px;
+    font-size: 14px;
+    color: ${palette.charcoal};
+  }
+  .sign-up-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-birthday-year-selector {
+      width: 33.33333%;
     }
   }
 `;
@@ -103,6 +134,24 @@ const SignUpModal = () => {
           onChange={onChangePassword}
           value={password}
         />
+      </div>
+
+      <p className="sign-up-birthdate-label">생일</p>
+      <p className="sign-up-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른 에어비앤비 이용자에게
+        공개되지 않습니다.
+      </p>
+
+      <div className="sign-up-birthday-selectors">
+        <div className="sign-up-birthday-month-selector">
+          <Selector options={monthList} disabledOptions={['월']} defaultValue="월" />
+        </div>
+        <div className="sign-up-birthday-day-selector">
+          <Selector options={dayList} disabledOptions={['일']} defaultValue="일" />
+        </div>
+        <div className="sign-up-birthday-year-selector">
+          <Selector options={yearList} disabledOptions={['년']} defaultValue="년" />
+        </div>
       </div>
     </ContainerForm>
   );
