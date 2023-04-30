955e6aee0926dd3a8dabb40b8148d115d667941a
sonncho <dungcho93@gmail.com>
2023-04-30 09:52:13 +0900

    fix: Redux wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import GlobalStyle from '../styles/GlobalStyles';
 import Header from '../components/Header';
 import { wrapper } from '../store';
 
-const app = ({ Component, pageProps }: AppProps) => {
+const app = ({ Component, ...rest }: AppProps) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <GlobalStyle />
       <Component {...pageProps} />
       <div id="root-modal" />
-    </>
+    </Provider>
   );
 };
 
-export default wrapper.withRedux(app);
+export default app;
