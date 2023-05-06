46cfa770ce8099a9161a65ac8e60c6866ff2b2cc
sonncho <dungcho93@gmail.com>
2023-05-06 09:46:58 +0900

    style: HamburgerMenu list styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
+import OutsideClickHandler from 'react-outside-click-handler';
 import AirbnbLogoIcon from '../public/static/svg/logo/logo.svg';
 import AirbnbLogoTextIcon from '../public/static/svg/logo/logo_text.svg';
 import palette from '../styles/palette';
@@ -84,12 +85,46 @@ const Container = styled.div`
       border-radius: 50%;
     }
   }
+
+  //** react-outside-click-handler div */
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
+    }
+  }
 `;
 
 const Header: React.FC = () => {
   const { openModalPortal, ModalPortal, closeModalPortal } = useModal();
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  const [isUserMenuOpend, setIsUserMenuOpend] = useState<boolean>(false);
 
   // console.log(user);
 
@@ -124,10 +159,34 @@ const Header: React.FC = () => {
         </div>
       )}
       {user.isLogged && ( // 로그인을 한경우
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img src={user.profileImage} className="header-user-profile-image" alt="" />
-        </button>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            if (isUserMenuOpend) {
+              setIsUserMenuOpend(false);
+            }
+          }}
+        >
+          <button
+            className="header-user-profile"
+            type="button"
+            onClick={() => setIsUserMenuOpend(!isUserMenuOpend)}
+          >
+            <HamburgerIcon />
+            <img src={user.profileImage} className="header-user-profile-image" alt="" />
+          </button>
+          {isUserMenuOpend && (
+            <ul className="header-usermenu">
+              <li>숙소 관리</li>
+              <li>
+                <Link href="/room/register/building">숙소 등록하기</Link>
+              </li>
+              <div className="header-usermenu-divider" />
+              <li role="presentation" onClick={() => {}}>
+                로그아웃
+              </li>
+            </ul>
+          )}
+        </OutsideClickHandler>
       )}
 
       <ModalPortal>
--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -11,6 +11,10 @@ const globalStyles = css`
     font-family: Noto Sans, Noto Sans KR;
     color: ${palette.black};
   }
+  a {
+    text-decoration: none;
+    color: ${palette.black};
+  }
 `;
 
 const GlobalStyle = createGlobalStyle`
