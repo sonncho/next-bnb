8abbed32dee22cdc9d81b2a8862220fe0be167b2
sonncho <dungcho93@gmail.com>
2023-05-14 18:30:54 +0900

    style: register room footer component styling

---
+++ components/register/RegisterRoomFooter.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import React from 'react';
+import styled from 'styled-components';
+import BackArrowIcon from '../../public/static/svg/register/register_room_footer_back_arrow.svg';
+import Button from '../common/Button';
+import palette from '../../styles/palette';
+
+const Container = styled.footer`
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 548px;
+  height: 82px;
+  padding: 14px 30px 20px;
+  background-color: white;
+  z-index: 10;
+  border-top: 1px solid ${palette.gray_dd};
+  .register-room-footer-back {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      margin-right: 8px;
+    }
+  }
+`;
+
+interface IProps {
+  prevHref?: string;
+  nextHref?: string;
+  isValid?: boolean;
+}
+
+const RegisterRoomFooter: React.FC<IProps> = ({ prevHref, nextHref, isValid = true }) => {
+  const onClickNext = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e);
+  };
+  return (
+    <Container>
+      <Link href={prevHref || ''}>
+        <span className="register-room-footer-back">
+          <BackArrowIcon />
+          뒤로
+        </span>
+      </Link>
+      <Link href={nextHref || ''}>
+        <Button color="dark_cyan" onClick={onClickNext}>
+          계속
+        </Button>
+      </Link>
+    </Container>
+  );
+};
+
+export default RegisterRoomFooter;
