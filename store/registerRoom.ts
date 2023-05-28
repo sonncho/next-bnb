4c678c4c97a9b9179a17250b1af3564c5d66ec44
sonncho <dungcho93@gmail.com>
2023-05-28 12:20:52 +0900

    style: add RegisterRoom page style

--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -1,10 +1,16 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { BedType } from '../types/room';
 
 type RegisterRoomState = {
   largeBuildingType: string | null;
   buildingType: string | null;
   roomType: string | null;
   isSetUpForGuest: boolean | null;
+  maximumGuestCount: number;
+  bedroomCount: number;
+  bedCount: number;
+  bedList: { id: number; beds: { type: BedType; count: number }[] }[];
+  publicBedList: { type: BedType; count: number }[];
 };
 
 //* 초기상태
@@ -13,6 +19,11 @@ const initialState: RegisterRoomState = {
   buildingType: null, //- 건물 유형(소분류)
   roomType: null, //- 숙소 유형
   isSetUpForGuest: null, //- 게스트만을 위해 만들어진 숙소인가?
+  maximumGuestCount: 1, //- 최대 숙박 인원
+  bedroomCount: 0, //- 침실 개수
+  bedCount: 1, //-침대 개수
+  bedList: [], //- 침대 유형
+  publicBedList: [], //-공용공간 침대 유형
 };
 
 const registerRoom = createSlice({
