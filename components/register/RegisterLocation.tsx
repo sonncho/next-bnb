c6411b878a73ae97d77389d50ee351f4d66d6ee9
sonncho <dungcho93@gmail.com>
2023-07-15 03:51:24 +0900

    style: add country selectbox and static country data

--- components/register/RegisterLocation.tsx
+++ components/register/RegisterLocation.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 import palette from '../../styles/palette';
+import Button from '../common/Button';
+import NavigationIcon from '../../public/static/svg/register/navigation.svg';
+import Selector from '../common/Selector';
+import { countryList } from '../../lib/staticData';
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -20,6 +24,14 @@ const Container = styled.div`
     max-width: 400px;
     margin-bottom: 24px;
   }
+  .register-rooom-location-button-wrapper {
+    width: 176px;
+    margin-bottom: 24px;
+  }
+  .register-room-location-country-selector-wrapper {
+    width: 385px;
+    margin-bottom: 24px;
+  }
 `;
 
 const RegisterLocation = () => {
@@ -30,6 +42,20 @@ const RegisterLocation = () => {
       <p className="register-room-step-info">
         정확한 숙소 주소는 게스트가 예약을 완료한 후에만 공개됩니다.
       </p>
+      <div className="register-rooom-location-button-wrapper">
+        <Button color="dark_cyan" colorReverse icon={<NavigationIcon />}>
+          현재 위치 사용
+        </Button>
+      </div>
+      <div className="register-room-location-country-selector-wrapper">
+        <Selector
+          type="register"
+          options={countryList}
+          useValidation={false}
+          defaultValue="국가/지역 선택"
+          disabledOptions={['국가/지역 선택']}
+        />
+      </div>
     </Container>
   );
 };
